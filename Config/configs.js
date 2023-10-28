8c535443837dbc421b223034d52d6ec3b0a79e25
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2023-10-28 13:08:39 +0330

    add database config in project

--- Config/configs.js
+++ Config/configs.js
@@ -1,8 +1,10 @@
 import {configDotenv} from "dotenv";
 import {paths} from "./paths.js";
+import {connection} from "./database.js";
 
-export let configs = () => {
+export let configs = (app) => {
 
     configDotenv({path : paths.general.env})
+    connection()
 
 }
