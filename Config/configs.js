cd78729e65e2559eb744cd5933c69fac5f547829
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2023-11-08 09:52:39 +0330

    add general api in config

--- Config/configs.js
+++ Config/configs.js
@@ -1,14 +1,21 @@
+import express, {urlencoded} from "express";
 import {configDotenv} from "dotenv";
+
 import {paths} from "./paths.js";
 import {connection} from "./database.js";
-import routers from "../Routes/api.js";
+
+import api_routes from "../Routes/api.js";
+import general_routes from "../Routes/general.js";
+
+import {authentication} from "../Middlewares/authentication.js";
 
 export let configs = (app) => {
 
     configDotenv({path : paths.general.env})
     connection()
 
-
-    app.use('/api' , routers)
+    app.use(express.json());
+    app.use('/' , general_routes)
+    app.use('/api' , authentication , api_routes)
 
 }
