a4db8c0052c871d47a6eccaf391e219188386596
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2023-11-11 10:02:05 +0330

    add messages in authentication middleware

--- Middlewares/authentication.js
+++ Middlewares/authentication.js
@@ -6,11 +6,11 @@ export let authentication = (req , res , next) => {
     let token = req.headers.authorization
     if (token){
         JsonWebToken.verify(token.split(" ")[1], process.env.JWT_SECRET, async (err, value) => {
-            if (err) return res.status(500).json({error: 'احراز هویت با موفقیت انجام نشد .'})
+            if (err) return res.status(500).json({error: req.t('authentication_failed')})
             req.user = await UserModel.findOne({email: value.email}).select(['email', 'username'])
             next()
         })
     }else {
-        return Responder.error(res , {message : "لطفا توکن را وارد کنید"})
+        return Responder.error(res , {message : req.t('token')})
     }
 }
